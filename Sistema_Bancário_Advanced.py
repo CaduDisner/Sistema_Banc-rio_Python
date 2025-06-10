134356e12758336c0dd36b4aa73261d6cb6b547a
CaduDisner <disnercadu@gmail.com>
2025-06-10 15:16:13 -0300

    Update Sistema_Bancário_Advanced.py

--- Sistema_Bancário_Advanced.py
+++ Sistema_Bancário_Advanced.py
@@ -1,1 +1,134 @@
+from abc import ABC, abtsractclassmethod, abstractproperty
+from datetime import datetime
+
+class Cliente:
+    def __init__(self, endereco):
+        self.endereco = endereco
+        self.contas = []
+  
+    def realizaar_transacao(self, conta, transacao):
+        transacao.registrar(conta)
+  
+    def adicionar_conta(self, conta):
+        self.contas.append(conta)
+
+class Cliente:
+    def __init__(self, nome, data_nascimento, cpf, endereco):
+        super().__init__(endereco)
+        self.nome = nome
+        self.data_nascimento = data_nascimento
+        self.cpf = cpf
+
+class Conta:
+  def __init__(self, numero, client):
+      self._saldo = 0
+      self._numero = numero 
+      self._agencia = "0001"
+      self._client = cliente
+      self._historico = Historico()
+
+  @classmethod
+  def nova_conta(cls, cliente, numero):
+      return cls(numero, cliente)
+
+  @property
+  def saldo(self:
+      return self._saldo
+
+  @property
+  def numero(self):
+      return self._numero
+
+  @property
+  def agencia(self):
+      return self._agencia
+
+  @property
+  def cliente(self):
+      return self._cliente
+
+  @property
+  def historico(self):
+      return self._historico
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
